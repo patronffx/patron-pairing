248baffaa68add3512e1766354318eb32b07428b
patronffx <168548862+patronffx@users.noreply.github.com>
2026-01-18 14:45:59 +0100

    Change browser user agent in socket creation

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ async function startnigg(phone) {
       const negga = Baileys.makeWASocket({
         printQRInTerminal: false,
         logger: pino({ level: 'silent' }),
-        browser: ["Ubuntu", "Opera", "100.0.4815.0"],
+        browser: ['Wileys', 'Desktop', '3.0'],
         auth: state,
         version: [2, 3000, 1027934701],
       });
